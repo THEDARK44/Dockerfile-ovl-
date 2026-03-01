18fcd0057bd9a7b7040cb13c8a9b0e3f7d74c79e
THEDARK44 <kra747634@gmail.com>
2026-03-01 20:31:31 +0000

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    OWNER_NUMBER: '0100268983',
+    botname: 'Sakura 🌸🌸',
+    ownername: 'THE_DARK',
+    SESSION_ID: process.env.SESSION_ID || '', // Laisse Render gérer ça
+};
+module.exports = config;
