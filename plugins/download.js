6a5e5ccb85c58586aa785f75103afe6a8e09a69a
THEDARK44 <kra747634@gmail.com>
2026-03-01 21:56:16 +0000

    Delete plugins directory

--- plugins/download.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    command: ["tiktok", "ytmp3", "ytmp4", "play"],
-    exec: async (sock, from, mek, args) => {
-        if (!args[0]) return sock.sendMessage(from, { text: "📌 Donnez-moi un lien, Chef !" });
-        await sock.sendMessage(from, { text: "⏳ *Sakura récupère le média...* cela prendra quelques secondes." });
-        // L'intégration des API de téléchargement se fera sur l'hébergeur pour plus de vitesse.
-    }
-}
-
--- plugins/sticker.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    command: ["s", "sticker"],
-    exec: async (sock, from, mek, args) => {
-        await sock.sendMessage(from, { text: "🌸 Envoi de l'image pour conversion..." }, { quoted: mek });
-        // Code de conversion ici...
-    }
-}
-
