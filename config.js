5f358481ee651530c325991d9ea2ab48bbea09a4
THEDARK44 <kra747634@gmail.com>
2026-03-01 21:14:09 +0000

    Add PREFIXE property to config
    Added PREFIXE property to config object.

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const config = {
     OWNER_NUMBER: '0100268983',
     botname: 'Sakura 🌸🌸',
     ownername: 'THE_DARK',
-    SESSION_ID: process.env.SESSION_ID || '', // Laisse Render gérer ça
+    PREFIXE: '.',
 };
 module.exports = config;
